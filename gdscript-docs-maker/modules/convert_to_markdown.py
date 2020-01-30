7b60d23f25d8f609dd0e4ec9d8067162ea2c217a
Nathan Lovato <nathan@gdquest.com>
2020-01-29 22:09:16 -0600

    Refactor code to encapsulate GDScript dataclasses
    Split the object creation and the conversion to markdown into two modules

--- gdscript-docs-maker/modules/convert_to_markdown.py
+++ gdscript-docs-maker/modules/convert_to_markdown.py
@@ -2,6 +2,8 @@
 from dataclasses import dataclass
 from typing import List
 
+from .gdscript_objects import GDScriptClass
+
 
 @dataclass
 class MarkdownDocument:
@@ -10,155 +12,6 @@ class MarkdownDocument:
 
     def get_filename(self):
         return self.title + ".md"
-
-
-@dataclass
-class Argument:
-    name: str
-    type: str
-
-
-@dataclass
-class Method:
-    signature: str
-    name: str
-    description: str
-    return_type: str
-    arguments: List[Argument]
-    rpc_mode: int
-
-    def summarize(self) -> List[str]:
-        return [self.return_type, self.signature]
-
-
-@dataclass
-class StaticFunction:
-    signature: str
-    name: str
-    description: str
-    return_type: str
-    arguments: List[Argument]
-
-    def summarize(self) -> List[str]:
-        return [self.return_type, self.signature]
-
-
-@dataclass
-class Member:
-    """Represents a property or member variable"""
-
-    signature: str
-    name: str
-    description: str
-    type: str
-    default_value: str
-    is_exported: bool
-    setter: str
-    getter: str
-
-    def summarize(self) -> List[str]:
-        return [self.type, self.name]
-
-
-@dataclass
-class GDScriptClass:
-    name: str
-    extends: str
-    description: str
-    path: str
-    methods: List[Method]
-    members: List[Member]
-    static_functions: List[StaticFunction]
-
-    def as_markdown(self) -> MarkdownDocument:
-        return MarkdownDocument(
-            [
-                *make_heading(self.name, 1),
-                make_bold("Extends:") + " " + self.extends_as_string(),
-                *make_heading("Description", 2),
-                self.description,
-                # Overview of the properties and methods
-                *make_heading("Properties", 2),
-                *self.summarize_members(),
-                *make_heading("Methods", 2),
-                *self.summarize_methods(),
-                *make_heading("Signals", 2),
-                *make_heading("Enumerations", 2),
-                # Full reference for the properties and methods.
-                *make_heading("Property Descriptions", 2),
-                *make_heading("Method Descriptions", 2),
-            ],
-            self.name,
-        )
-
-    def extends_as_string(self) -> str:
-        return " < ".join(self.extends)
-
-    def summarize_members(self) -> List[str]:
-        if not self.members:
-            return []
-        header: List[str] = make_table_header(["Type", "Name"])
-        return header + [make_table_row(member.summarize()) for member in self.members]
-
-    def summarize_methods(self) -> List[str]:
-        header: List[str] = make_table_header(["Type", "Name"])
-        return header + [make_table_row(method.summarize()) for method in self.methods]
-
-
-def _get_methods(data: List[dict]) -> List[Method]:
-    methods: List[Method] = []
-    for entry in data:
-        method: Method = Method(
-            entry["signature"],
-            entry["name"],
-            entry["description"].strip(" \n"),
-            entry["return_type"],
-            _get_arguments(entry["arguments"]),
-            entry["rpc_mode"],
-        )
-        methods.append(method)
-    return methods
-
-
-def _get_arguments(data: List[dict]) -> List[Argument]:
-    arguments: List[Argument] = []
-    for entry in data:
-        argument: Argument = Argument(
-            entry["name"], entry["type"],
-        )
-        arguments.append(argument)
-    return arguments
-
-
-def _get_members(data: List[dict]) -> List[Member]:
-    members: List[Member] = []
-    for entry in data:
-        member: Member = Member(
-            entry["signature"],
-            entry["name"],
-            entry["description"].strip(" \n"),
-            entry["data_type"],
-            entry["default_value"],
-            entry["export"],
-            entry["setter"],
-            entry["getter"],
-        )
-        members.append(member)
-    return members
-
-
-def _get_static_functions(data: List[dict]) -> List[StaticFunction]:
-    static_functions: List[StaticFunction] = []
-    for entry in data:
-        static_function: StaticFunction = StaticFunction(
-            entry["signature"],
-            entry["name"],
-            entry["description"].strip(" \n"),
-            entry["return_type"],
-            _get_arguments(entry["arguments"]),
-        )
-        static_functions.append(static_function)
-    return static_functions
 
 
 def convert_to_markdown(data: dict = {}) -> List[MarkdownDocument]:
@@ -172,16 +25,41 @@ def convert_to_markdown(data: dict = {}) -> List[MarkdownDocument]:
 
 
 def _convert_class_to_markdown(data: dict = {}) -> MarkdownDocument:
-    gdscript_class: GDScriptClass = GDScriptClass(
-        data["name"],
-        data["extends_class"],
-        data["description"].strip(" \n"),
-        data["path"],
-        _get_methods(data["methods"]),
-        _get_members(data["members"]),
-        _get_static_functions(data["static_functions"]),
+    return as_markdown(GDScriptClass.from_dict(data))
+
+
+def as_markdown(gdscript: GDScriptClass) -> MarkdownDocument:
+    return MarkdownDocument(
+        [
+            *make_heading(gdscript.name, 1),
+            make_bold("Extends:") + " " + gdscript.extends_as_string(),
+            *make_heading("Description", 2),
+            gdscript.description,
+            # Overview of the properties and methods
+            *make_heading("Properties", 2),
+            *summarize_members(gdscript),
+            *make_heading("Methods", 2),
+            *summarize_methods(gdscript),
+            *make_heading("Signals", 2),
+            *make_heading("Enumerations", 2),
+            # Full reference for the properties and methods.
+            *make_heading("Property Descriptions", 2),
+            *make_heading("Method Descriptions", 2),
+        ],
+        gdscript.name,
     )
-    return gdscript_class.as_markdown()
+
+
+def summarize_members(gdscript: GDScriptClass) -> List[str]:
+    if not gdscript.members:
+        return []
+    header: List[str] = make_table_header(["Type", "Name"])
+    return header + [make_table_row(member.summarize()) for member in gdscript.members]
+
+
+def summarize_methods(gdscript: GDScriptClass) -> List[str]:
+    header: List[str] = make_table_header(["Type", "Name"])
+    return header + [make_table_row(method.summarize()) for method in gdscript.methods]
 
 
 def make_heading(line: str, level: int = 1) -> List[str]:
---
+++ gdscript-docs-maker/modules/gdscript_objects.py
@@ -0,0 +1,134 @@
+"""Converts the json representation of GDScript classes as dictionaries into objects
+"""
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Argument:
+    name: str
+    type: str
+
+
+@dataclass
+class Method:
+    signature: str
+    name: str
+    description: str
+    return_type: str
+    arguments: List[Argument]
+    rpc_mode: int
+
+    def summarize(self) -> List[str]:
+        return [self.return_type, self.signature]
+
+
+@dataclass
+class StaticFunction:
+    signature: str
+    name: str
+    description: str
+    return_type: str
+    arguments: List[Argument]
+
+    def summarize(self) -> List[str]:
+        return [self.return_type, self.signature]
+
+
+@dataclass
+class Member:
+    """Represents a property or member variable"""
+
+    signature: str
+    name: str
+    description: str
+    type: str
+    default_value: str
+    is_exported: bool
+    setter: str
+    getter: str
+
+    def summarize(self) -> List[str]:
+        return [self.type, self.name]
+
+
+@dataclass
+class GDScriptClass:
+    name: str
+    extends: str
+    description: str
+    path: str
+    methods: List[Method]
+    members: List[Member]
+    static_functions: List[StaticFunction]
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return GDScriptClass(
+            data["name"],
+            data["extends_class"],
+            data["description"].strip(" \n"),
+            data["path"],
+            _get_methods(data["methods"]),
+            _get_members(data["members"]),
+            _get_static_functions(data["static_functions"]),
+        )
+
+    def extends_as_string(self) -> str:
+        return " < ".join(self.extends)
+
+
+def _get_methods(data: List[dict]) -> List[Method]:
+    methods: List[Method] = []
+    for entry in data:
+        method: Method = Method(
+            entry["signature"],
+            entry["name"],
+            entry["description"].strip(" \n"),
+            entry["return_type"],
+            _get_arguments(entry["arguments"]),
+            entry["rpc_mode"],
+        )
+        methods.append(method)
+    return methods
+
+
+def _get_arguments(data: List[dict]) -> List[Argument]:
+    arguments: List[Argument] = []
+    for entry in data:
+        argument: Argument = Argument(
+            entry["name"], entry["type"],
+        )
+        arguments.append(argument)
+    return arguments
+
+
+def _get_members(data: List[dict]) -> List[Member]:
+    members: List[Member] = []
+    for entry in data:
+        member: Member = Member(
+            entry["signature"],
+            entry["name"],
+            entry["description"].strip(" \n"),
+            entry["data_type"],
+            entry["default_value"],
+            entry["export"],
+            entry["setter"],
+            entry["getter"],
+        )
+        members.append(member)
+    return members
+
+
+def _get_static_functions(data: List[dict]) -> List[StaticFunction]:
+    static_functions: List[StaticFunction] = []
+    for entry in data:
+        static_function: StaticFunction = StaticFunction(
+            entry["signature"],
+            entry["name"],
+            entry["description"].strip(" \n"),
+            entry["return_type"],
+            _get_arguments(entry["arguments"]),
+        )
+        static_functions.append(static_function)
+    return static_functions
