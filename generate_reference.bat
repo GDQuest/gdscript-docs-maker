36663648b2ec1c722ca2162f98c31ce60d88362b
Francois Belair <razoric480@gmail.com>
2020-02-02 12:34:04 -0500

    Make windows batch more in line with shell script

--- generate_reference.bat
+++ generate_reference.bat
@@ -5,9 +5,25 @@
 
 :: Configuration: Modify ReferenceCollectorCLI.gd with paths relative to the project's res://
 
-IF NOT EXIST "%1\project.godot" (
-	echo Godot project not found at %~dp1project.godot
-	EXIT /B -1	
+where /q godot*
+if ERRORLEVEL 1 (
+	ECHO "Godot is missing from the PATH environment."
+	EXIT /B
+)
+
+where /q python*
+IF ERRORLEVEL 1 (
+	ECHO "Python is missing from the PATH environment."
+	EXIT /B
+)
+
+for /f "delims=" %%F in ('where godot*') do set godot=%%F
+
+set project_path=%1
+
+IF NOT EXIST "%project_path%\project.godot" (
+	echo Could not find a project.godot in %project_path%. This program needs a Godot project to work.
+	EXIT /B
 )
 
 IF [%2] == [] (
@@ -16,51 +32,63 @@ IF [%2] == [] (
 	set project_name=%2
 )
 
-IF NOT EXIST %project_name% (
+IF NOT EXIST "%project_name%" (
 	:: Creates the directory with project name.
-	mkdir %project_name%
+	mkdir "%project_name%"
 )
 
-echo Copying collector
+set gdscript_path=godot-scripts
+set gdscript_1=ReferenceCollectorCLI.gd
+set gdscript_2=Collector.gd
+
 :: Copies the CLI gdscript to the project location so godot can find it in res://
-copy /Y godot-scripts\ReferenceCollectorCLI.gd %1\ReferenceCollectorCLI.gd
-copy /Y godot-scripts\Collector.gd %1\Collector.gd
+copy /Y "%gdscript_path%\%gdscript_1%" "%project_path%\ReferenceCollectorCLI.gd" >nul
+copy /Y "%gdscript_path%\%gdscript_2%" "%project_path%\Collector.gd" >nul
 
-echo Running godot
+echo Generating reference...
 :: Runs godot in editor mode, runs the script ReferenceCollectorCLI, and quits
-godot -e -q -s --no-window --path %1 ReferenceCollectorCLI.gd >nul
+%godot% -e -q -s --no-window --path "%project_path%" ReferenceCollectorCLI.gd >nul
 
-echo Cleaning up
 :: Removes the CLI tool from the project.
-erase /Q %1\ReferenceCollectorCLI.gd
-erase /Q %1\Collector.gd
+erase /Q "%project_path%\ReferenceCollectorCLI.gd"
+erase /Q "%project_path%\Collector.gd"
 
-IF NOT EXIST %1\reference.json (
-	echo Collector failed - Json reference file not created
-	EXIT /B -1
+IF NOT EXIST "%project_path%\reference.json" (
+	echo There was an error generating the reference from godot.
+	EXIT /B
+) else (
+	echo Done.
 )
 
-echo Preparing for distribution
-:: Moves the dumped json file.
-move /Y %1\reference.json %project_name%\reference.json
+:: Empty current version of dist if it exists
+if EXIST dist (
+	erase /Q dist
+)
 
+echo Generating markdown files in %project_name%
 :: Runs the markdown creator.
 :: TODO: Skip the moving by having the python script output dist into the project folder
-python -m gdscript-docs-maker %project_name%/reference.json
+python -m gdscript-docs-maker "%project_path%/reference.json"
 
 IF NOT EXIST dist (
+	goto :PythonError
+) else (
+	for /F %%i in ('dir /b "dist\*.*"') do (
+		goto :Success
+	)
+	
+	:PythonError
 	echo Python module failed to create markdown distribution
-	EXIT /B -1
+	EXIT /B
 )
 
-IF EXIST %project_name%\dist (
+:Success
+IF EXIST "%project_name%" (
 	:: Move all files in dist into project name dist
-	erase /Q %project_name%\dist
-	move /Y dist\* %project_name%\dist >nul
+	erase /Q "%project_name%"
+	move /Y dist\* "%project_name%" >nul
 	rmdir /q /s dist
 ) ELSE (
 	:: Puts the resulting distribution result into the project folder.
-	move /Y dist %project_name%\dist
+	move /Y dist "%project_name%"
 )
-
-echo Done. Output now in %~dp0%project_name%\dist
