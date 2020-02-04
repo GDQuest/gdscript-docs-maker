e6d678d130ca04fda1e8f10bab363ff9b0bfdff4
Nathan Lovato <nathan@gdquest.com>
2020-02-03 18:57:03 -0600

    Add help message to generate_reference.sh

--- generate_reference.sh
+++ generate_reference.sh
@@ -3,24 +3,53 @@
 project_directory=$1
 output_directory=$2
 
+echo_help() {
+    echo "
+Generate a code reference from GDScript
+Usage:
+generate_reference.sh $project_directory (optional)$output_directory
+
+Arguments:
+
+$project_directory -- path to your Godot project directory.
+This directory or one of its subdirectories should contain a
+project.godot file.
+
+$output_directory -- directory path to output the documentation into.
+
+Flags:
+
+-h/--help -- Display this help message.
+"
+    exit 0
+}
+
+if [ $1 = "-h" ]
+then echo_help
+fi
+if [ $1 = "--help" ]
+then echo_help
+fi
+if [ $1 = "help" ]
+then echo_help
+fi
+
 # Testing input parameters
 if [ -z $project_directory ]
 then echo Missing first parameter: project_directory
    exit 1
 fi
-if [ -z $output_directory ]
-then echo Missing second parameter: output_directory
-     exit 1
-fi
 if ! test -d $project_directory
 then echo Directory $project_directory does not exist, exiting.
      exit 1
 fi
-
 godot_project_file=`find $project_directory -iname project.godot -print -quit`
 if ! test -f $godot_project_file
 then echo "Could not find a project.godot file in $target_directory. This program needs a Godot project to work."
      exit 1
+fi
+if [ -z $output_directory ]
+then output_directory="dist"
 fi
 
 # Generate reference JSON data from Godot
