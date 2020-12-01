b20ce81e68d4c6d68be4756ef074da72311be4c3
Divided by Zer0 <mail@dbzer0.com>
2020-12-01 02:20:40 +0100

    strips trailing whitespace still

--- gdscript_docs_maker/modules/gdscript_objects.py
+++ gdscript_docs_maker/modules/gdscript_objects.py
@@ -69,7 +69,7 @@ Metadata should be of the form key: value, e.g. category: Category Name
         elif match_category:
             category = match_category.group(1)
         else:
-            description_trimmed.append(re.sub(r'^ ([^\s])',r'\g<1>',line))
+            description_trimmed.append(re.sub(r'^ ([^\s])',r'\g<1>',line).rstrip())
 
     metadata: Metadata = Metadata(tags, category)
     return "\n".join(description_trimmed), metadata
